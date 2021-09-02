2461708bf9a31a88cd0cb4a21e1992fdb6bc4b6a
Hung Bang <hung.bang@evizi.com>
2021-09-02 16:42:58 +0700

    initial project

---
+++ src/main/java/cafe/javawritter/hikaricpexample/HikaricpExampleApplication.java
@@ -0,0 +1,13 @@
+package cafe.javawritter.hikaricpexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikaricpExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikaricpExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cafe/javawritter/hikaricpexample/HikaricpExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package cafe.javawritter.hikaricpexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikaricpExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
